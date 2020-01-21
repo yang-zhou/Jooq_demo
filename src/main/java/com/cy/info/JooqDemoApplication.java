4985d9aee250cc794f0ed0f758f4fdc9bbf0336e
yangzhou <zhou.yang@changhong.com>
2020-01-21 15:31:03 +0800

    1、初始化springboot项目 2、引入jooq gradle代码生成器插件 3、配置插件并生成相关代码

---
+++ src/main/java/com/cy/info/JooqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cy.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqDemoApplication.class, args);
+	}
+
+}
